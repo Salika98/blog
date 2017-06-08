1df3a1f5269d1a14dab87a2cbf4e1e9971abcaf3
Salika98 <zoritonozlieva@gmail.com>
2017-06-08 14:16:30 +0300

    Update login.php

--- views/users/login.php
+++ views/users/login.php
@@ -1,5 +1,5 @@
 <?php $this->title = 'Login'; ?>
-
+<link rel="stylesheet" type="text/css" href="<?=APP_ROOT?>/content/styles.css">
 <form id="specialform" method="post">
 <h1><?= htmlspecialchars($this->title) ?></h1>
 	<div>Username: <input type="text" name="username"></div>
