759192107881b40573fd5bf5c7b0b2d6031cc5ac
Salika98 <zoritonozlieva@gmail.com>
2017-06-08 14:13:57 +0300

    Update create.php

--- views/posts/create.php
+++ views/posts/create.php
@@ -1,5 +1,5 @@
 <?php $this->title = 'Create a new post'; ?>
-
+<link rel="stylesheet" type="text/css" href="<?=APP_ROOT?>/content/styles.css">
 <form id="specialform" method="post">
 	<h1><?=htmlspecialchars($this->title)?></h1>
 	<div>Title: </div>
