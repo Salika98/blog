050835bd49f7df59f2c621de3982282577d8d634
Salika98 <zoritonozlieva@gmail.com>
2017-06-08 14:14:26 +0300

    Update edit.php

--- views/posts/edit.php
+++ views/posts/edit.php
@@ -1,5 +1,5 @@
 <?php $this->title = 'Edit existing post'; ?>
-
+<link rel="stylesheet" type="text/css" href="<?=APP_ROOT?>/content/styles.css">
 <h1><?=htmlspecialchars($this->title)?></h1>
 
 <form method="post">
